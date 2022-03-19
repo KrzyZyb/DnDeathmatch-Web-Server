148c6ba0b066b7986450d6ea168613ddb44a75c3
KZYB <krzysztof.zybul@gmail.com>
2022-03-19 15:31:30 +0100

    Adding edges to fields

--- DnDeathmatch/src/main/java/com/dndeathmatch/game/gameplay/model/arena/Field.java
+++ DnDeathmatch/src/main/java/com/dndeathmatch/game/gameplay/model/arena/Field.java
@@ -1,20 +1,36 @@
 package com.dndeathmatch.game.gameplay.model.arena;
 
+import org.springframework.security.core.parameters.P;
+
 import java.awt.*;
 
 public class Field {
-    private Point point;
-    private FieldType fieldType;
+    private final Point point;
+    private final Point[] edges;
+    private final FieldType fieldType;
 
     private Field(FieldType fieldType, Point point){
         this.fieldType = fieldType;
         this.point = point;
+        this.edges = this.setEdges(point);
+    }
+
+    private Point[] setEdges(Point point) {
+        Point bottomLeft = point;
+        Point upperLeft = new Point(point.x, point.y+1);
+        Point upperRight = new Point(point.x+1, point.y+1);
+        Point bottomRight = new Point(point.x+1, point.y);
+        return new Point[]{bottomLeft, upperLeft, upperRight, bottomRight};
     }
 
     public static Field getFieldByType(int fieldTypeId, Point point){
         //GET FIELD TYPE BY ORDINAL
         FieldType fieldType = FieldType.values()[fieldTypeId];
         return new Field(fieldType, point);
+    }
+
+    public Point[] getEdges(){
+        return edges;
     }
 
     @Override
