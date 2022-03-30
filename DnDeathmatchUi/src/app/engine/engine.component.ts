cc2a3aa796d763a3db4e398b5294fe1681f53edd
KZYB <krzysztof.zybul@gmail.com>
2022-03-30 19:16:46 +0200

    Adding excalibur, updating angular

--- DnDeathmatchUi/src/app/engine/engine.component.ts
+++ DnDeathmatchUi/src/app/engine/engine.component.ts
@@ -1,5 +1,4 @@
 import { OnInit, Component, ElementRef, NgZone } from '@angular/core';
-import { Application, Container, Sprite, Texture } from 'pixi.js';
 
 @Component({
   selector: 'app-engine',
@@ -7,24 +6,13 @@ import { Application, Container, Sprite, Texture } from 'pixi.js';
   styleUrls: ['./engine.component.scss']
 })
 export class EngineComponent implements OnInit {
-  public app: Application;
 
   constructor(private elementRef: ElementRef, private ngZone: NgZone) {}
 
   ngOnInit(): void {
-    this.ngZone.runOutsideAngular(() => {
-      this.app = new Application({});
-    });
-    this.elementRef.nativeElement.appendChild(this.app.view);
-    this.updateState();
   }
 
   updateState():void{
-    const bunny = Sprite.from('../assets/class_wizard.png');
-    bunny.anchor.set(0.5);
-    bunny.x = this.app.screen.width / 2;
-    bunny.y = this.app.screen.height / 2;
-    this.app.stage.addChild(bunny);
   }
 
 }
