fae3743898edab21dcde90d7fceae014e3a613a9
KZYB <krzysztof.zybul@gmail.com>
2022-04-26 21:32:46 +0200

    Increase of performance budget, adding excalibur window draft

--- DnDeathmatchUi/src/app/engine/engine.component.ts
+++ DnDeathmatchUi/src/app/engine/engine.component.ts
@@ -1,4 +1,5 @@
 import { OnInit, Component, ElementRef, NgZone } from '@angular/core';
+import { Actor, CollisionType, Color, Engine } from 'excalibur';
 
 @Component({
   selector: 'app-engine',
@@ -7,12 +8,35 @@ import { OnInit, Component, ElementRef, NgZone } from '@angular/core';
 })
 export class EngineComponent implements OnInit {
 
-  constructor(private elementRef: ElementRef, private ngZone: NgZone) {}
+  constructor(private elementRef: ElementRef, private ngZone: NgZone) { }
 
   ngOnInit(): void {
+    const game = new Engine({
+      width: 800,
+      height: 600,
+    });
+    game.start();
+    const paddle = new Actor({
+      x: 150,
+      y: game.drawHeight - 40,
+      width: 200,
+      height: 20,
+      // Let's give it some color with one of the predefined
+      // color constants
+      color: Color.Chartreuse,
+    });
+
+    // Make sure the paddle can partipate in collisions, by default excalibur actors do not collide with each other
+    // CollisionType.Fixed is like an object with infinite mass, and cannot be moved, but does participate in collision.
+    paddle.body.collisionType = CollisionType.Fixed;
+
+    // `game.add` is the same as calling
+    // `game.currentScene.add`
+    game.add(paddle);
   }
 
-  updateState():void{
+
+  updateState(): void {
   }
 
 }
