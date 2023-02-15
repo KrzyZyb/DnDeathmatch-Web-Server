0e032fa332c0a4a94a1bd16e5f3764794d6ba280
Krzysztof Zybul <krzysztof.zybul@god.dev>
2023-02-15 10:33:15 +0100

    Cross Origin solved

--- src/main/java/com/dndeathmatch/game/controllers/Communication.java
+++ src/main/java/com/dndeathmatch/game/controllers/Communication.java
@@ -6,9 +6,11 @@ import com.dndeathmatch.game.gameplay.Game;
 import com.dndeathmatch.game.gameplay.model.Player;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class Communication {
     private Game game;
 
